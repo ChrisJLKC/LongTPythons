e8586808d5f1b8afbc29ca36465952b6d29b2939
Christopher Jenner <chris.jenner1234@gmail.com>
2019-08-06 17:00:06 +0100

    Updated pumping scripts

--- PumpingTPythons.py
+++ PumpingTPythons.py
@@ -1,10 +1,10 @@
-from gpiozero import Motor
+from gpiozero.pins.mock import MockFactory
+from gpiozero import Device, Motor
 from time import sleep
 
+Device.pin_factory = MockFactory()
 
 pump = Motor(17, 18)
 pump.forward()
-
 sleep(5)
-
 pump.stop()

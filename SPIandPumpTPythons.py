fa5446f79073c504de3ad82c39958e762c64fe7f
Ishaq <io143599@ghs.truropenwith.ac.uk>
2019-08-07 12:04:29 +0100

    SPI and Pump Code

---
+++ SPIandPumpTPythons.py
@@ -0,0 +1,26 @@
+from gpiozero import Device, Motor
+from time import sleep
+import spidev
+
+bus = 0
+device = 1
+pump = Motor(17, 18)
+
+pump_time = 2
+cycle_time = 10800
+number_of_cycles = 8
+
+spi = spidev.SpiDev()
+spi.open(bus, device)
+
+# Settings (for example)
+spi.max_speed_hz = 5000
+spi.mode = 0b01
+
+
+for i in range(1,number_of_cycles):
+        pump.forward()
+        sleep(pump_time)
+        pump.stop()
+        sleep(cycle_time)
+

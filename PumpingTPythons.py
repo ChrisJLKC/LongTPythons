6f5f4a658b9279c9bad228896c46d6dfcbb6a8bd
Zachary Pinder <zac.pinder@gmail.com>
2019-08-07 12:01:18 +0100

    moved code into class

--- PumpingTPythons.py
+++ PumpingTPythons.py
@@ -1,13 +1,16 @@
 from gpiozero import Device, Motor
 from time import sleep
 
-pump_time = 2
-cycle_time = 10800
-number_of_cycles = 8
-pump = Motor(17, 18)
+class Schedule_Pumping:
 
-for i in range(1,number_of_cycles):
-        pump.forward()
-        sleep(pump_time)
-        pump.stop()
-        sleep(cycle_time)
+        def __init__(self, number_of_cycles = 8, cycle_time = 10800, pump_time = 2):
+                pump_time = 2
+                cycle_time = 10800
+                number_of_cycles = 8
+                pump = Motor(17, 18)
+
+                for i in range(1,number_of_cycles):
+                        pump.forward()
+                        sleep(pump_time)
+                        pump.stop()
+                        sleep(cycle_time)

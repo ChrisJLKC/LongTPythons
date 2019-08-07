65de1c6aef920d5b62cace283fad91328d6b6e95
Christopher Jenner <chris.jenner1234@gmail.com>
2019-08-07 16:40:24 +0100

    Test updated

--- Test_PumpingTPythons.py
+++ Test_PumpingTPythons.py
@@ -3,5 +3,5 @@ import PumpingTPythons
 #Sample test to test tests test    
 def test_Number_Of_Cycles_Is_8():
     '''This test will make sure the number of cycles is always 8'''
-    PumpCycles = PumpingTPythons.Schedule_Pumping
-    assert PumpCycles.pump_time() == 2
+    PumpCycles = PumpingTPythons.Schedule_Pumping()
+    assert PumpCycles.moisture_level() == 0

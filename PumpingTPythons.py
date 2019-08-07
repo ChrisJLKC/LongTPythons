8743a0b95725012a892061be1140c6adb5367a96
Zachary Pinder <zac.pinder@gmail.com>
2019-08-07 11:32:30 +0100

    Moved pump setup out of loop

--- PumpingTPythons.py
+++ PumpingTPythons.py
@@ -4,9 +4,9 @@ from time import sleep
 pump_time = 2
 cycle_time = 10800
 number_of_cycles = 8
+pump = Motor(17, 18)
 
 for i in range(1,number_of_cycles):
-        pump = Motor(17, 18)
         pump.forward()
         sleep(pump_time)
         pump.stop()

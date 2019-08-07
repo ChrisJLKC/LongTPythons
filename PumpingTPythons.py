7d78fc3790d13fd78f64c090fefebc7cfa8579be
Zachary Pinder <zac.pinder@gmail.com>
2019-08-07 11:16:02 +0100

    Corrected error, used value in stead of variable

--- PumpingTPythons.py
+++ PumpingTPythons.py
@@ -8,7 +8,7 @@ pump_time = 2
 cycle_time = 10800
 number_of_cycles = 8
 
-for i in range(1,8):
+for i in range(1,number_of_cycles):
         pump = Motor(17, 18)
         pump.forward()
         sleep(pump_time)

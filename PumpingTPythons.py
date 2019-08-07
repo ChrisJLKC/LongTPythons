75b8e1f45ff3f33eaeca401e28c2b6aa0fc2b2df
Ishaq <io143599@ghs.truropenwith.ac.uk>
2019-08-07 16:05:11 +0100

    Cleaned up pump script

--- PumpingTPythons.py
+++ PumpingTPythons.py
@@ -4,22 +4,20 @@ import spidev
 
 class Schedule_Pumping:
 
-    bus = 19
-    device = 21
-
-    spi = spidev.SpiDev()
-    spi.open(0, 0)
-
-    spi.max_speed_hz = 5000
-    spi.mode = 0b01
-
-    pump = Motor(17, 18)
-
     def __init__(self, number_of_cycles = 8, cycle_time = 10800, pump_time = 2, min_moisture = 100):
-        moisture_level_p = spi.xfer([0b01100000, 0b00000000])
-        moisture_level = (moisture_level_p[0] * 256) + moisture_level_p
-
         
+        bus = 19
+        device = 21
+        spi = spidev.SpiDev()
+        spi.open(0, 0)
+        spi.max_speed_hz = 5000
+        spi.mode = 0b01
+        
+        pump = Motor(17, 18)        
+        
+        moisture_level_p = spi.xfer([0b01100000, 0b00000000])
+        moisture_level = (moisture_level_p[0] * 256) + moisture_level_p[1]
+
         for _ in range(1,number_of_cycles):
             if moisture_level < min_moisture:
                 pump.forward()

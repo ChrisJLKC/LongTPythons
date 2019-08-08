1b5b10bdb996d111ff6f50bd0bcdc2e6f4f7e95d
Ishaq <io143599@ghs.truropenwith.ac.uk>
2019-08-08 11:54:31 +0100

    Testing and gerneral fix

--- Main.py
+++ Main.py
@@ -1,21 +1,27 @@
+from time import time
 import PumpingTPythons
 
-number_of_cycles = 8
+max_run_time = 24
+max_run_time = max_run_time * 60 * 60
 cycle_time = 10800
-thirsty_time = 600 
+thirsty_time = 600
 
 min_moisture = 400
 pump_time = 2
 
-pump = PumpingTPythons.Schedule_Pumping
+pump = PumpingTPythons.Schedule_Pumping()
 
-for _ in range(1,number_of_cycles):
+time_run = 0
+while time_run < max_run_time:
     if pump.pump_water(pump_time, min_moisture) == True:
-        between_pumping(cycle_time)
+        pump.between_pumping(cycle_time)
+        time_run += 10800
     elif pump.pump_water(pump_time, min_moisture) == False:
-        between_pumping(thirsty_time)
-    
+        pump.between_pumping(thirsty_time)
+        time_run += 600
+        
     #placeholder :)
     else:
         pass
+
     
--- PumpingTPythons.py
+++ PumpingTPythons.py
@@ -12,18 +12,18 @@ class Schedule_Pumping:
     
     def moisture_check(self):
         #Sets up the SPI values required to check moisture - returns the value as a positive integer
-        spi.open(0, 0)
-        spi.max_speed_hz = 5000
-        spi.mode = 0b01
-        moisture_level_p = spi.xfer([0b01100000, 0b00000000]) #This is the 16 bit binary number that is produced from the 8-bit chip 
+        self.spi.open(0, 0)
+        self.spi.max_speed_hz = 5000
+        self.spi.mode = 0b01
+        moisture_level_p = self.spi.xfer([0b01100000, 0b00000000]) #This is the 16 bit binary number that is produced from the 8-bit chip 
         return (moisture_level_p[0] * 256) + moisture_level_p[1] #combines both 8 bit binary numbers into a 16 bit number
     
     def pump_water(self, pump_time, min_moisture):
         #Causes the device to stop/start pumping water based on the current moisture level
-        if moisture_check() < min_moisture:
-            pump.forward()
+        if self.moisture_check() < min_moisture:
+            self.pump.forward()
             sleep(pump_time)
-            pump.stop()
+            self.pump.stop()
             return True
         else:
             return False

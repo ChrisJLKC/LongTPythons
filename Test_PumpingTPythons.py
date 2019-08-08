9765d733f85a03745c827c30c450af8d8ba28a0f
Christopher Jenner <chris.jenner1234@gmail.com>
2019-08-08 11:08:08 +0100

    Added Tests for PumpingTPythons

--- Test_PumpingTPythons.py
+++ Test_PumpingTPythons.py
@@ -1,7 +1,14 @@
 import PumpingTPythons
 
 #Sample test to test tests test    
-def test_Number_Of_Cycles_Is_8():
-    '''This test will make sure the number of cycles is always 8'''
-    PumpCycles = PumpingTPythons.Schedule_Pumping()
-    assert PumpCycles.moisture_level == 0
+def test_moisture_level():
+    pump = PumpingTPythons.Schedule_Pumping()
+    result = pump.moisture_check()
+    assert type(result) is int
+    assert result >= 0
+    
+def test_pump_water():
+    pump = PumpingTPythons.Schedule_Pumping()
+    assert type(pump.pump_water(2, 1)) is bool
+
+    

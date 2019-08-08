30fb3206432e297413e07d50d7c881d759a30fe3
Ishaq <io143599@ghs.truropenwith.ac.uk>
2019-08-08 11:31:29 +0100

    Improved Main

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import PumpingTPythons
 
 number_of_cycles = 8
 cycle_time = 10800
+thirsty_time = 600 
 
 min_moisture = 400
 pump_time = 2
@@ -9,7 +10,12 @@ pump_time = 2
 pump = PumpingTPythons.Schedule_Pumping
 
 for _ in range(1,number_of_cycles):
-    pump.pump_water(pump_time, min_moisture)
-    between_pumping(cycle_time)
+    if pump.pump_water(pump_time, min_moisture) == True:
+        between_pumping(cycle_time)
+    elif pump.pump_water(pump_time, min_moisture) == False:
+        between_pumping(thirsty_time)
     
+    #placeholder :)
+    else:
+        pass
     

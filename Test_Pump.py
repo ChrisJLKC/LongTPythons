9fa6799bf445f1986e3717de605fafd16411980a
ChrisJLKC <chris.jenner1234@gmail.com>
2019-08-09 15:06:04 +0100

    Test code for pumping code

--- Test_Pump.py
+++ Test_Pump.py
@@ -1,14 +1,9 @@
-import PumpingTPythons
+import Pump
 
-#Sample test to test tests test    
-def test_moisture_level_is_positive_interger():
-    pump = PumpingTPythons.Schedule_Pumping()
-    result = pump.moisture_check()
-    assert type(result) is int
-    assert result >= 0
+def test_pump_total_time():
+    pump = Pump.Pump_Control()
+    assert pump.pump_water(2, 0.1) == 3
+    assert pump.pump_water(2, 2) == 22
     
-def test_pump_water_returns_boolean():
-    pump = PumpingTPythons.Schedule_Pumping()
-    assert type(pump.pump_water(2, 1)) is bool
-
+    
     

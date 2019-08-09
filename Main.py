64c77487be088b821b4688fee0abbeec3cc01b16
ChrisJLKC <chris.jenner1234@gmail.com>
2019-08-09 12:27:41 +0100

    Added new variables and while loop

--- Main.py
+++ Main.py
@@ -1,26 +1,17 @@
 import PumpingTPythons
+import SensorTPythons
 
-max_run_time = 24
-max_run_time = max_run_time * 60 * 60
-cycle_time = 10800
-thirsty_time = 600
-
+watering_time = 2
+plant_height = 0.10
 min_moisture = 800
-pump_time = 2
 
-pump = PumpingTPythons.Schedule_Pumping()
+time_between_water = 180
 
-time_run = 0
-while time_run < max_run_time:
-    if pump.pump_water(pump_time, min_moisture) == True:
-        pump.between_pumping(cycle_time)
-        time_run += 10800
-    elif pump.pump_water(pump_time, min_moisture) == False:
-        pump.between_pumping(thirsty_time)
-        time_run += 600
-        
-    #placeholder :)
-    else:
-        pass
+pump = PumpingTPythons.Pump_Control()
+sensor = SensorTPythons.Sensor_Control()
+
+while 
+
+
 
     

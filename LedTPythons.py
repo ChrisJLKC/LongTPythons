448d382ba785fba7f79edb82daa2aee2af5b6469
Ishaq <io143599@ghs.truropenwith.ac.uk>
2019-08-09 14:42:42 +0100

    Deleted dupe files

--- LedTPythons.py
+++
@@ -1,8 +0,0 @@
-from gpiozero import LED #Imports the LED library
-from time import sleep # Imports the sleep library
-
-led = LED(17) #Defines the pin the LED will be using (17)
-
-led.on() #Turns the LED on
-sleep(1) #Pause the process for 1 second
-led.off() #Turn the LED off

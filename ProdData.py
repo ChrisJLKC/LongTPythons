69e4e78e7d11ea7f378341eda0cb9f5d1a8e88a7
Ishaq <io143599@ghs.truropenwith.ac.uk>
2019-08-14 15:18:02 +0100

    Changed the order that data is written to csv

--- ProdData.py
+++ ProdData.py
@@ -5,7 +5,8 @@ from datetime import datetime
 class Data_Handling:
 
     def write_to_csv(self, data):
-        row = [datetime.now(), *data[0], data[1]]
+        sensordata = (data[0][0], data[0][2], data[0][1])
+        row = [datetime.now(), *sensordata, data[1]]
         with open('data.csv', 'a') as data_file:
             csv_writer = csv.writer(data_file)
             csv_writer.writerow(row)

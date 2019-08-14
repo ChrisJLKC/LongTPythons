e8d34af8cca93c491530e6d7fdc13b2684e4e658
Ishaq <io143599@ghs.truropenwith.ac.uk>
2019-08-14 15:11:09 +0100

    No longer writing a tuple to csv. Now all tuple values are individual elements

--- ProdData.py
+++ ProdData.py
@@ -4,8 +4,8 @@ from datetime import datetime
 
 class Data_Handling:
 
-    def write_to_csv(self, moisture_level):
-        row = [datetime.now(), moisture_level]
+    def write_to_csv(self, data):
+        row = [datetime.now(), *data[0], data[1]]
         with open('data.csv', 'a') as data_file:
             csv_writer = csv.writer(data_file)
             csv_writer.writerow(row)
